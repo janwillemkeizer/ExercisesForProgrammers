b4a2407a3e98b7cc696679d46485b822cba56ee4
Jan Willem Keizer <jan.willem.keizer@newskool.nl>
2019-10-21 18:46:13 +0200

    Initial commit

---
+++ src/nl/janwillemkeizer/Main.java
@@ -0,0 +1,18 @@
+package nl.janwillemkeizer;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + ", nice to meet you!");
+
+        scanner.close();
+    }
+}
