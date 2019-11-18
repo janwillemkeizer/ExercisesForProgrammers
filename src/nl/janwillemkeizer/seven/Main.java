80019fb025b164e33c272d92c57a558b40795bd8
Jan Willem Keizer <jan.willem.keizer@newskool.nl>
2019-11-18 14:38:47 +0100

    Typo correction.

--- src/nl/janwillemkeizer/seven/Main.java
+++ src/nl/janwillemkeizer/seven/Main.java
@@ -32,15 +32,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("What is the length of the room in feet?");
-        int lenghtInFeet = scanner.nextInt();
+        int lengthInFeet = scanner.nextInt();
 
         System.out.println("What is the width of the room in feet?");
         int widthInFeet = scanner.nextInt();
 
-        int squareFeet = lenghtInFeet * widthInFeet;
+        int squareFeet = lengthInFeet * widthInFeet;
         float squareMeters = squareFeet * conversionFactor;
 
-        System.out.printf("You entered dimensions of %d feet by %d feet.\n", lenghtInFeet, widthInFeet);
+        System.out.printf("You entered dimensions of %d feet by %d feet.\n", lengthInFeet, widthInFeet);
         System.out.printf("The area is\n%d square feet \n%.3f square meters", squareFeet, squareMeters);
 
         scanner.close();
